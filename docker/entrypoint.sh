4e8fad7eea69de9366a3ee7274d757929c707147
DiegoBrine269 <diegooloarte269@gmail.com>
2026-03-21 11:54:07 -0600

    Ensure logs directory exists in entrypoint script

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,4 +5,6 @@ php artisan config:cache
 php artisan route:cache
 php artisan migrate --force
 
+mkdir -p /var/www/html/storage/logs
+
 exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
