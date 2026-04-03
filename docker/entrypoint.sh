04e10e24dfb9bfba216483c7f6355a5142bf6774
DiegoBrine269 <diegooloarte269@gmail.com>
2026-04-02 23:17:53 -0600

    Add missing directory creation and permissions for Nginx client body

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -9,4 +9,8 @@ mkdir -p /var/www/html/storage/logs
 chown -R www-data:www-data /var/www/html/storage
 chmod -R 775 /var/www/html/storage
 
+mkdir -p /var/lib/nginx/tmp/client_body
+chown -R www-data:www-data /var/lib/nginx/tmp
+chmod -R 700 /var/lib/nginx/tmp
+
 exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
